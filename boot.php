486f8b6b25ab400fa31cb2d6628da343d6773d79
Thomas Skerbis <skerbis@klxm.de>
2022-05-30 17:34:51 +0200

    2.0.0-dev

--- boot.php
+++ boot.php
@@ -1,33 +1,67 @@
 <?php
-
-/**
- * accessdenied Addon.
- *
- * @author Friends Of REDAXO
- *
- * @var rex_addon
- */
+// Add status for locked articles and categories
 if (rex::isBackend()) {
     rex_extension::register(['ART_STATUS_TYPES', 'CAT_STATUS_TYPES'], function (rex_extension_point $ep) {
         $subject = $ep->getSubject();
-        $subject[] = array(rex_i18n::msg('accessdenied_blocked'), 'rex-offline', 'rex-icon-offline');
+        $subject[] = array(rex_i18n::msg('access_denied_locked'), 'rex-offline', 'fa fa-exclamation-triangle');
         $ep->setSubject($subject);
-
         return $ep->getSubject();
     });
-    if (rex_addon::get('quick_navigation')->isAvailable()) {
-        rex_view::addCssFile($this->getAssetsUrl('accessdenied_quicknavi.css'));
-    }
+        if (rex_addon::get('quick_navigation')->isAvailable()) {
+        rex_view::addCssFile($this->getAssetsUrl('access_denied_qn.css'));
+      }
+}
 
-    // set a default status on new articles
-    rex_extension::register('ART_ADDED', ['accessdenied', 'setDefaultArticleStatus']);
-    rex_extension::register('CAT_ADDED', ['accessdenied', 'setDefaultCategoryStatus']);
-
-} else {
+// redirect to not foundArticle if not logged in or preview parameter not set. 
+if (rex::isFrontend()) {
     rex_extension::register('PACKAGES_INCLUDED', function () {
-        if (rex_article::getCurrent() instanceof rex_article && rex_article::getCurrent()->getValue('status') == 2 && !rex_backend_login::hasSession()) {
+       $package = rex_addon::get('access_denied');   
+       if ($package->getConfig('inherit') == true &&  rex_category::getCurrent() != null) {
+            $cat = rex_category::getCurrent();
+            if ($cat->getClosest(fn (rex_category $cat) => 2 == $cat->getValue('status')) && rex_request('preview', 'string', '')  != 'id-' . rex_article::getCurrent()->getId()  && !rex_backend_login::hasSession()) {
+                rex_response::setStatus(rex_response::HTTP_MOVED_TEMPORARILY);
+                rex_redirect(rex_article::getNotfoundArticleId(), rex_clang::getCurrentId());
+            }
+        }
+        if (rex_article::getCurrent() instanceof rex_article && rex_request('preview', 'string', '') != 'id-' . rex_article::getCurrent()->getId() && rex_article::getCurrent()->getValue('status') == 2 && !rex_backend_login::hasSession()) {
             rex_response::setStatus(rex_response::HTTP_MOVED_TEMPORARILY);
             rex_redirect(rex_article::getNotfoundArticleId(), rex_clang::getCurrentId());
         }
     }, rex_extension::LATE);
 }
+
+if (rex::isBackend()) {
+    
+    // set a default status on new articles
+    rex_extension::register('ART_ADDED', ['accessdenied', 'setDefaultArticleStatus']);
+    rex_extension::register('CAT_ADDED', ['accessdenied', 'setDefaultCategoryStatus']);
+
+    $catclocked = false;
+    $cat = rex_category::getCurrent();
+    $package = rex_addon::get('access_denied');  
+    if ($package->getConfig('inherit') == true && $cat && $cat->getClosest(fn (rex_category $cat) => 2 == $cat->getValue('status')))
+    {
+        $catclocked = true; 
+    } 
+    $art = rex_article::getCurrent();
+    if ($art && $art->getValue('status') == 2 || true == $catclocked) {
+        rex_extension::register('STRUCTURE_CONTENT_SIDEBAR', function (rex_extension_point $ep) {
+            $params = $ep->getParams();
+            $subject = $ep->getSubject();
+
+            $panel = '<div class="alert alert-info">'.rex_i18n::msg('access_denied_share').'<br><strong>' . rex_yrewrite::getFullUrlByArticleId($params["article_id"]) . '?preview=id-' . rex_article::getCurrent()->getId() . '</strong></div>';
+
+            $fragment = new rex_fragment();
+            $fragment->setVar('title', '<i class="fa fa-exclamation-triangle" style="color: red"></i> '.rex_i18n::msg('access_denied_info'), false);
+            $fragment->setVar('body', $panel, false);
+            $fragment->setVar('article_id', $params["article_id"], false);
+
+            $fragment->setVar('collapse', true);
+            $fragment->setVar('collapsed', false);
+            $content = $fragment->parse('core/page/section.php');
+
+            return $subject . $content;
+        });
+    }
+}
+
--- pages/index.php
+++ pages/index.php
@@ -1,20 +1,25 @@
 <?php
-
-echo rex_view::title($this->i18n('accessdenied_title'));
-
-$form = rex_config_form::factory('accessdenied');
-
+echo rex_view::title($this->i18n('access_denied_title'));
+$form = rex_config_form::factory('access_denied');
 $field = $form->addSelectField('default_status');
-$field->setLabel($this->i18n('accessdeined_settings_default_status_label'));
+$field->setLabel($this->i18n('access_denied_settings_default_status_label'));
 $select = $field->getSelect();
 $select->setSize(1);
-$select->addOption($this->i18n('accessdeined_settings_default_status_offline'), '0');
-$select->addOption($this->i18n('accessdeined_settings_default_status_online'), '1');
-$select->addOption($this->i18n('accessdeined_settings_default_status_blocked'), '2');
-$field->setNotice($this->i18n('accessdeined_settings_default_status_notice'));
+$select->addOption($this->i18n('access_denied_settings_default_status_offline'), '0');
+$select->addOption($this->i18n('access_denied_settings_default_status_online'), '1');
+$select->addOption($this->i18n('access_denied_settings_default_status_blocked'), '2');
+$field->setNotice($this->i18n('access_denied_settings_default_status_notice'));
+
+$field = $form->addSelectField('inherit');
+$field->setLabel($this->i18n('access_denied_settings_inherit_label'));
+$select = $field->getSelect();
+$select->setSize(1);
+$select->addOption($this->i18n('access_denied_settings_inherit_off'), false);
+$select->addOption($this->i18n('access_denied_settings_inherit_on'), true);
+
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
-$fragment->setVar('title', $this->i18n('accessdeined_settings'), false);
+$fragment->setVar('title', $this->i18n('access_denied_settings'), false);
 $fragment->setVar('body', $form->get(), false);
 echo $fragment->parse('core/page/section.php');
