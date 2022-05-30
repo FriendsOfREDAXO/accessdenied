8668c6e7304cecbe3d3684554ceb1dd69844079a
Thomas Skerbis <skerbis@klxm.de>
2022-05-30 21:26:19 +0200

    linkparameter

--- pages/system.accessdenied.php
+++ pages/system.accessdenied.php
@@ -1,5 +1,4 @@
 <?php
-
 $form = rex_config_form::factory('accessdenied');
 $field = $form->addSelectField('default_status');
 $field->setLabel($this->i18n('accessdenied_settings_default_status_label'));
@@ -17,9 +16,16 @@ $select->setSize(1);
 $select->addOption($this->i18n('accessdenied_settings_inherit_off'), false);
 $select->addOption($this->i18n('accessdenied_settings_inherit_on'), true);
 
+$field = $form->addInputField('text', 'linkparameter', null, ['class' => 'form-control']);
+$field->setLabel($this->i18n('linkparameter'));
+$field->getValidator()->add('notEmpty', $this->i18n('linkparameter_empty'));
+$field->getValidator()->add( 'match',  $this->i18n('linkparameter_wrong'), '/^[a-zA-Z]+$/');
+
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
 $fragment->setVar('title', $this->i18n('accessdenied_settings'), false);
 $fragment->setVar('body', $form->get(), false);
 echo $fragment->parse('core/page/section.php');
+
+
