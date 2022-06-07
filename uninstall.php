07edac8eb462a93bd4a1a57ec9e2c5648eeac1bf
Thomas Skerbis <skerbis@klxm.de>
2022-06-07 12:55:57 +0200

    accessdenied_uninstall_message"

--- uninstall.php
+++ uninstall.php
@@ -3,3 +3,5 @@
 $update = rex_sql::factory();
 $update->setQuery('UPDATE '.rex::getTablePrefix().'article SET status=0 WHERE status = 2');
 rex_addon::get('structure')->clearCache();
+$package = rex_addon::get('accessdenied');
+$package->setProperty('successmsg', '<br><strong>' . rex_i18n::msg("accessdenied_uninstall_message") . '</strong>');
