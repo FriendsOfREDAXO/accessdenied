90c55ba8f96e68cfee731955c049be307f0132b6
Alexander Walther <github.com@alexplus.de>
2023-12-17 18:05:56 +0100

    allow `[a-zA-Z0-9_.-]`
    closes #40

--- pages/system.accessdenied.php
+++ pages/system.accessdenied.php
@@ -19,7 +19,7 @@ $select->addOption($this->i18n('accessdenied_settings_inherit_on'), true);
 $field = $form->addInputField('text', 'linkparameter', null, ['class' => 'form-control']);
 $field->setLabel($this->i18n('linkparameter'));
 $field->getValidator()->add('notEmpty', $this->i18n('linkparameter_empty'));
-$field->getValidator()->add( 'match',  $this->i18n('linkparameter_wrong'), '/^[a-zA-Z]+$/');
+$field->getValidator()->add( 'match',  $this->i18n('linkparameter_wrong'), '/^[a-zA-Z0-9_.-]+$/');
 
 
 $fragment = new rex_fragment();
